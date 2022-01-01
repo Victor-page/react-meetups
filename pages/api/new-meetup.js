4501845c7051d41f7fe6e394f65d9d75c9d53aaf
Victor <victor.shevchuk.96@gmail.com>
2022-01-01 13:31:49 +0200

    connected to mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,24 @@
 // /api/new-meetup
 // POST /api/new-meetup
 
-const handler = (request, response) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (request, response) => {
   if (request.method !== 'POST') {
     return;
   }
 
   const data = request.body;
-  const { title, image, address, description } = data;
+  // const { title, image, address, description } = data;
+
+  const client = await MongoClient.connect();
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+  const result = await meetupsCollection.insertOne(data);
+  console.log(result);
+  client.close();
+
+  response.status(201).json({ message: 'Meetup inserted!', result, data });
 };
 
 export default handler;
