10b3cbe660a6e656aedc6c2de106cd66633cb393
Victor <victor.shevchuk.96@gmail.com>
2022-01-03 10:10:29 +0200

    commented out  the env vars

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -6,13 +6,13 @@ import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 const NewMeetUpPage = () => {
   const router = useRouter();
 
-  const addMeetupHandler = async (enteredMeetupData) => {
+  async function addMeetupHandler(enteredMeetupData) {
     // get the current environment
-    const dev = process.env.NODE_ENV !== 'production';
-    const { DEV_URL, PROD_URL } = process.env;
-    const api = `${dev ? DEV_URL : PROD_URL}/api/new-meetup`;
+    // const dev = process.env.NODE_ENV !== 'production';
+    // const { DEV_URL, PROD_URL } = process.env;
+    // const api = `${dev ? DEV_URL : PROD_URL}/api/new-meetup`;
 
-    const response = await fetch(api, {
+    const response = await fetch('/api/new-meetup', {
       method: 'POST',
       body: JSON.stringify(enteredMeetupData),
       headers: { 'Content-Type': 'application/json' },
@@ -22,7 +22,7 @@ const NewMeetUpPage = () => {
     // console.log(data);
 
     router.push('/');
-  };
+  }
 
   return (
     <Fragment>
